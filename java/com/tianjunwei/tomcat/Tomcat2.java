6f5536eac0bde6c195170ee17b4d2cb97eb67b1b
tianjunwei <924862077@qq.com>
2018-03-20 11:14:36 +0800

    修改注释

--- java/com/tianjunwei/tomcat/Tomcat2.java
+++ java/com/tianjunwei/tomcat/Tomcat2.java
@@ -24,52 +24,60 @@ public class Tomcat2 {
 	public static void main(String[] args) {
 		System.setProperty("catalina.base", System.getProperty("user.dir"));
 		System.err.println("start TestTomcat");
+		//创建连接器
 		Connector connector = new Connector("HTTP/1.1");
 		connector.setPort(8080);
 		
+		//创建上下文容器
 	    Context context = new StandardContext();
 	    context.setPath("/context");
-	    //context.setDocBase("app1");
 
+	    //创建servlet包装类
 	    Wrapper defaultServlet = context.createWrapper();
 		defaultServlet.setName("test");
 		defaultServlet.setServletClass("com.tianjunwei.servlet.TestServlet");
 		defaultServlet.setLoadOnStartup(1);
 		defaultServlet.setOverridable(true);
+		
+		//上下文容器中添加servlet
 		context.addChild(defaultServlet);
 		context.addServletMappingDecoded("/test", "test");
 		context.addLifecycleListener(new FixContextListener());
 	    
+		//创建host
 	    Host host = new StandardHost();
 	    host.addChild(context);
 	    host.setName("localhost");
 	    host.setAppBase(System.getProperty("user.dir") + File.separator + ".");
 	    
+	    //创建引擎
 	    Engine engine = new StandardEngine();
 	    engine.setName("Tomcat");
 	    engine.addChild(host);
 	    engine.setDefaultHost("localhost");
 	    engine.getCatalinaBase();
 
+	    //创建一个服务，作为服务器server中的一个服务
 	    Service service = new StandardService();
 	    service.setName("Tomcat");
 		service.setContainer(engine );
 		service.addConnector(connector);
 		
+		//创建服务器
 	    Server server = new StandardServer();
 	    server.setPort(-1);
 	    server.addService(service);
 	    server.setCatalinaBase(new File(System.getProperty("user.dir")));
 	    server.setCatalinaHome(new File(System.getProperty("user.dir")));
 
-	    // Start the new server
 	    if (server instanceof Lifecycle) {
 	      try {
+	    	//初始化服务
 	        server.init();
+	        //启动服务
 	        ((Lifecycle) server).start();
+	        //挂起服务
 	        server.await();
-	        // the program waits until the await method returns,
-	        // i.e. until a shutdown command is received.
 	      }
 	      catch (LifecycleException e) {
 	        e.printStackTrace(System.out);
